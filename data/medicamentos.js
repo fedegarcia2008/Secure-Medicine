3c7892b4d0139d558223c584321fec8b0790c9c2
fedegarcia2008 <garcia.federico@tecnica7.edu.ar>
2026-09-15 21:13:02 -0300

    Lista de medicamentos

---
+++ data/medicamentos.js
@@ -0,0 +1,404 @@
+
+
+export const medicamentos = [
+  {
+    id: '1',
+    nombre: 'Paracetamol',
+    categoria: 'Analgésico'
+  },
+  {
+    id: '2',
+    nombre: 'Ibuprofeno',
+    categoria: 'Analgésico'
+  },
+  {
+    id: '3',
+    nombre: 'Aspirina',
+    categoria: 'Analgésico'
+  },
+  {
+    id: '4',
+    nombre: 'Diclofenac',
+    categoria: 'Antiinflamatorio'
+  },
+  {
+    id: '5',
+    nombre: 'Naproxeno',
+    categoria: 'Antiinflamatorio'
+  },
+  {
+    id: '6',
+    nombre: 'Ketorolac',
+    categoria: 'Analgésico'
+  },
+  {
+    id: '7',
+    nombre: 'Ketoprofeno',
+    categoria: 'Antiinflamatorio'
+  },
+  {
+    id: '8',
+    nombre: 'Meloxicam',
+    categoria: 'Antiinflamatorio'
+  },
+  {
+    id: '9',
+    nombre: 'Tramadol',
+    categoria: 'Analgésico'
+  },
+  {
+    id: '10',
+    nombre: 'Omeprazol',
+    categoria: 'Gastrointestinal'
+  },
+  {
+    id: '11',
+    nombre: 'Pantoprazol',
+    categoria: 'Gastrointestinal'
+  },
+  {
+    id: '12',
+    nombre: 'Esomeprazol',
+    categoria: 'Gastrointestinal'
+  },
+  {
+    id: '13',
+    nombre: 'Metoclopramida',
+    categoria: 'Antiemético'
+  },
+  {
+    id: '14',
+    nombre: 'Domperidona',
+    categoria: 'Gastrointestinal'
+  },
+  {
+    id: '15',
+    nombre: 'Butilhioscina',
+    categoria: 'Antiespasmódico'
+  },
+  {
+    id: '16',
+    nombre: 'Loperamida',
+    categoria: 'Antidiarreico'
+  },
+  {
+    id: '17',
+    nombre: 'Lactulosa',
+    categoria: 'Laxante'
+  },
+  {
+    id: '18',
+    nombre: 'Loratadina',
+    categoria: 'Antialérgico'
+  },
+  {
+    id: '19',
+    nombre: 'Cetirizina',
+    categoria: 'Antialérgico'
+  },
+  {
+    id: '20',
+    nombre: 'Desloratadina',
+    categoria: 'Antialérgico'
+  },
+  {
+    id: '21',
+    nombre: 'Levocetirizina',
+    categoria: 'Antialérgico'
+  },
+  {
+    id: '22',
+    nombre: 'Fexofenadina',
+    categoria: 'Antialérgico'
+  },
+  {
+    id: '23',
+    nombre: 'Salbutamol',
+    categoria: 'Respiratorio'
+  },
+  {
+    id: '24',
+    nombre: 'Budesonida',
+    categoria: 'Respiratorio'
+  },
+  {
+    id: '25',
+    nombre: 'Beclometasona',
+    categoria: 'Respiratorio'
+  },
+  {
+    id: '26',
+    nombre: 'Ambroxol',
+    categoria: 'Respiratorio'
+  },
+  {
+    id: '27',
+    nombre: 'Acetilcisteína',
+    categoria: 'Respiratorio'
+  },
+  {
+    id: '28',
+    nombre: 'Dextrometorfano',
+    categoria: 'Antitusivo'
+  },
+  {
+    id: '29',
+    nombre: 'Amoxicilina',
+    categoria: 'Antibiótico'
+  },
+  {
+    id: '30',
+    nombre: 'Azitromicina',
+    categoria: 'Antibiótico'
+  },
+  {
+    id: '31',
+    nombre: 'Claritromicina',
+    categoria: 'Antibiótico'
+  },
+  {
+    id: '32',
+    nombre: 'Cefalexina',
+    categoria: 'Antibiótico'
+  },
+  {
+    id: '33',
+    nombre: 'Ciprofloxacina',
+    categoria: 'Antibiótico'
+  },
+  {
+    id: '34',
+    nombre: 'Metronidazol',
+    categoria: 'Antibiótico'
+  },
+  {
+    id: '35',
+    nombre: 'Enalapril',
+    categoria: 'Antihipertensivo'
+  },
+  {
+    id: '36',
+    nombre: 'Losartán',
+    categoria: 'Antihipertensivo'
+  },
+  {
+    id: '37',
+    nombre: 'Amlodipina',
+    categoria: 'Antihipertensivo'
+  },
+  {
+    id: '38',
+    nombre: 'Atenolol',
+    categoria: 'Antihipertensivo'
+  },
+  {
+    id: '39',
+    nombre: 'Bisoprolol',
+    categoria: 'Antihipertensivo'
+  },
+  {
+    id: '40',
+    nombre: 'Furosemida',
+    categoria: 'Diurético'
+  },
+  {
+    id: '41',
+    nombre: 'Hidroclorotiazida',
+    categoria: 'Diurético'
+  },
+  {
+    id: '42',
+    nombre: 'Atorvastatina',
+    categoria: 'Colesterol'
+  },
+  {
+    id: '43',
+    nombre: 'Rosuvastatina',
+    categoria: 'Colesterol'
+  },
+  {
+    id: '44',
+    nombre: 'Simvastatina',
+    categoria: 'Colesterol'
+  },
+  {
+    id: '45',
+    nombre: 'Metformina',
+    categoria: 'Antidiabético'
+  },
+  {
+    id: '46',
+    nombre: 'Glibenclamida',
+    categoria: 'Antidiabético'
+  },
+  {
+    id: '47',
+    nombre: 'Gliclazida',
+    categoria: 'Antidiabético'
+  },
+  {
+    id: '48',
+    nombre: 'Levotiroxina',
+    categoria: 'Hormona tiroidea'
+  },
+  {
+    id: '49',
+    nombre: 'Vitamina C',
+    categoria: 'Vitamina'
+  },
+  {
+    id: '50',
+    nombre: 'Vitamina D',
+    categoria: 'Vitamina'
+  },
+  {
+    id: '51',
+    nombre: 'Vitamina B12',
+    categoria: 'Vitamina'
+  },
+  {
+    id: '52',
+    nombre: 'Ácido fólico',
+    categoria: 'Vitamina'
+  },
+  {
+    id: '53',
+    nombre: 'Hierro',
+    categoria: 'Suplemento'
+  },
+  {
+    id: '54',
+    nombre: 'Calcio',
+    categoria: 'Suplemento'
+  },
+  {
+    id: '55',
+    nombre: 'Magnesio',
+    categoria: 'Suplemento'
+  },
+  {
+    id: '56',
+    nombre: 'Sertralina',
+    categoria: 'Antidepresivo'
+  },
+  {
+    id: '57',
+    nombre: 'Escitalopram',
+    categoria: 'Antidepresivo'
+  },
+  {
+    id: '58',
+    nombre: 'Fluoxetina',
+    categoria: 'Antidepresivo'
+  },
+  {
+    id: '59',
+    nombre: 'Clonazepam',
+    categoria: 'Ansiolítico'
+  },
+  {
+    id: '60',
+    nombre: 'Alprazolam',
+    categoria: 'Ansiolítico'
+  },
+  {
+    id: '61',
+    nombre: 'Lorazepam',
+    categoria: 'Ansiolítico'
+  },
+  {
+    id: '62',
+    nombre: 'Mebendazol',
+    categoria: 'Antiparasitario'
+  },
+  {
+    id: '63',
+    nombre: 'Albendazol',
+    categoria: 'Antiparasitario'
+  },
+  {
+    id: '64',
+    nombre: 'Ivermectina',
+    categoria: 'Antiparasitario'
+  },
+  {
+    id: '65',
+    nombre: 'Clotrimazol',
+    categoria: 'Antifúngico'
+  },
+  {
+    id: '66',
+    nombre: 'Miconazol',
+    categoria: 'Antifúngico'
+  },
+  {
+    id: '67',
+    nombre: 'Aciclovir',
+    categoria: 'Antiviral'
+  },
+  {
+    id: '68',
+    nombre: 'Mupirocina',
+    categoria: 'Antibiótico tópico'
+  },
+  {
+    id: '69',
+    nombre: 'Betametasona',
+    categoria: 'Corticoide'
+  },
+  {
+    id: '70',
+    nombre: 'Prednisona',
+    categoria: 'Corticoide'
+  },
+  {
+    id: '71',
+    nombre: 'Dexametasona',
+    categoria: 'Corticoide'
+  },
+  {
+    id: '72',
+    nombre: 'Clopidogrel',
+    categoria: 'Antiagregante plaquetario'
+  },
+  {
+    id: '73',
+    nombre: 'Ácido acetilsalicílico',
+    categoria: 'Antiagregante plaquetario'
+  },
+  {
+    id: '74',
+    nombre: 'Alopurinol',
+    categoria: 'Antigotoso'
+  },
+  {
+    id: '75',
+    nombre: 'Colchicina',
+    categoria: 'Antigotoso'
+  },
+  {
+    id: '76',
+    nombre: 'Tamsulosina',
+    categoria: 'Urológico'
+  },
+  {
+    id: '77',
+    nombre: 'Finasterida',
+    categoria: 'Urológico'
+  },
+  {
+    id: '78',
+    nombre: 'Etinilestradiol + Levonorgestrel',
+    categoria: 'Anticonceptivo'
+  },
+  {
+    id: '79',
+    nombre: 'Etinilestradiol + Drospirenona',
+    categoria: 'Anticonceptivo'
+  },
+  {
+    id: '80',
+    nombre: 'Lágrimas artificiales',
+    categoria: 'Oftálmico'
+  }
+];
